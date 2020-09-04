abff86ed6108e9a937d4b9ea188b000435e49dd7
Mikhail1992 <misderkach@yandex.ru>
2020-09-05 00:34:33 +0300

    Add files command

--- src/gendiff.js
+++ src/gendiff.js
@@ -4,6 +4,12 @@ const start = () => {
   program
     .version("0.0.1")
     .description("Compares two configuration files and shows a difference.")
+    .arguments("filepath1 filepath2")
+    .option("-f, --format [type]", "output format")
+    .action((filepath1, filepath2) => {
+      console.log("filepath1", filepath1);
+      console.log("filepath2", filepath2);
+    })
     .parse(process.argv);
 };
 

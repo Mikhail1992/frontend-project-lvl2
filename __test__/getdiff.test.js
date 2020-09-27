8dbe7659c057aa666c83dd2c11f06e6089157b5d
Mikhail1992 <misderkach@yandex.ru>
2020-09-27 15:27:39 +0300

    Fix __dirname implementation

--- __test__/getdiff.test.js
+++ __test__/getdiff.test.js
@@ -1,24 +1,22 @@
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import gendiff from "../src/diff";
+import gendiff from '../src/diff';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.join(__dirname, "..", "__fixtures__", filename);
-const readFile = (filename) =>
-  fs.readFileSync(getFixturePath(filename), "utf-8");
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test("gendiff", () => {
+test('gendiff', () => {
   // arrange
-  const filepath1 = getFixturePath("json/1.json");
-  const filepath2 = getFixturePath("json/2.json");
+  const filepath1 = getFixturePath('json/1.json');
+  const filepath2 = getFixturePath('json/2.json');
 
   // act
-  const actual = readFile("json/result.txt");
+  const actual = readFile('json/result.txt');
 
   // assert
   expect(gendiff(filepath1, filepath2)).toEqual(actual);
